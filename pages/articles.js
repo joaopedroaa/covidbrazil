9326c0879c7d938657fd8c37ad599a24145bbe21
João Pedro Alvarenga <joaopedroaats@pm.me>
2020-11-05 20:19:41 -0300

    feat: add articles list

--- pages/articles.js
+++ pages/articles.js
@@ -6,10 +6,35 @@ import BackMenu from "../components/BackMenu"
 import FormatSvg from "../components/FormatSvg"
 
 // Styles
+import styles from '../styles/Articles.module.css'
 import stylesGeneral from '../styles/Styles.module.css'
 
 
 export default function artigos() {
+
+  const articles = [
+    {
+      image: "01-small.jpg",
+      author: "Joao",
+      date: "05 de maio de 2020",
+      title: "Lorem ipsum dolor sit amet",
+      subtitle: "ossimus sunt provident eve nihil quod quam, ossimus sunt provident eve nihil quod quam",
+    },
+    {
+      image: "02-small.jpg",
+      author: "Pedro",
+      date: "04 de maio de 2020",
+      title: "Lorem ipsum ",
+      subtitle: "ossimus sunt provident eve nihil quod quam, ossimus sunt provident eve nihil quod quam ossimus sunt provident eve nihil quod quam, ossimus sunt provident eve nihil quod quam",
+    },
+    {
+      image: "01-small.jpg",
+      author: "Joao",
+      date: "05 de maio de 2020",
+      title: "Lorem ipsum dolor sit amet",
+      subtitle: "ossimus sunt provident eve nihil quod quam, ossimus sunt provident eve nihil quod quam ossimus sunt provident eve nihil quod quam, ossimus sunt provident eve nihil quod quam ossimus sunt provident eve nihil quod quam, ossimus sunt provident eve nihil quod quam",
+    }
+  ]
 
   return (
     <div className={stylesGeneral.container}>
@@ -24,7 +49,23 @@ export default function artigos() {
 
 
 
-      {/* <Footer /> */}
+      <ul className={styles.unorderedList}>
+        {articles.map((article, code) => {
+          return (
+            <li className={styles.list}>
+              <img src={"/articles/" + article.image} alt="" />
+              <div className={styles.data}>
+                <p>{article.date}</p>
+                <h2>{article.title}</h2>
+                <h3>{article.subtitle}</h3>
+                <h4>{article.author}</h4>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+
+      <Footer />
     </div>
   )
 }
