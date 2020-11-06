146722afa028a65ce7882867487adda61d93ae26
João Pedro Alvarenga <joaopedroaats@pm.me>
2020-11-06 09:20:48 -0300

    feat: create a cover image articles

--- pages/articles.js
+++ pages/articles.js
@@ -9,32 +9,32 @@ import FormatSvg from "../components/FormatSvg"
 import styles from '../styles/Articles.module.css'
 import stylesGeneral from '../styles/Styles.module.css'
 
+const articles = [
+  {
+    image: "01-small.jpg",
+    author: "Joao",
+    date: "05 de maio de 2020",
+    title: "Lorem ipsum dolor sit amet",
+    subtitle: "ossimus sunt provident eve nihil quod quam, ossimus sunt provident eve nihil quod quam",
+  },
+  {
+    image: "02-small.jpg",
+    author: "Pedro",
+    date: "04 de maio de 2020",
+    title: "Lorem ipsum ",
+    subtitle: "ossimus sunt provident eve nihil quod quam, ossimus sunt provident eve nihil quod quam ossimus sunt provident eve nihil quod quam, ossimus sunt provident eve nihil quod quam",
+  },
+  {
+    image: "01-small.jpg",
+    author: "Joao",
+    date: "05 de maio de 2020",
+    title: "Lorem ipsum dolor sit amet",
+    subtitle: "ossimus sunt provident eve nihil quod quam, ossimus sunt provident eve nihil quod quam ossimus sunt provident eve nihil quod quam, ossimus sunt provident eve nihil quod quam ossimus sunt provident eve nihil quod quam, ossimus sunt provident eve nihil quod quam",
+  }
+]
 
 export default function artigos() {
 
-  const articles = [
-    {
-      image: "01-small.jpg",
-      author: "Joao",
-      date: "05 de maio de 2020",
-      title: "Lorem ipsum dolor sit amet",
-      subtitle: "ossimus sunt provident eve nihil quod quam, ossimus sunt provident eve nihil quod quam",
-    },
-    {
-      image: "02-small.jpg",
-      author: "Pedro",
-      date: "04 de maio de 2020",
-      title: "Lorem ipsum ",
-      subtitle: "ossimus sunt provident eve nihil quod quam, ossimus sunt provident eve nihil quod quam ossimus sunt provident eve nihil quod quam, ossimus sunt provident eve nihil quod quam",
-    },
-    {
-      image: "01-small.jpg",
-      author: "Joao",
-      date: "05 de maio de 2020",
-      title: "Lorem ipsum dolor sit amet",
-      subtitle: "ossimus sunt provident eve nihil quod quam, ossimus sunt provident eve nihil quod quam ossimus sunt provident eve nihil quod quam, ossimus sunt provident eve nihil quod quam ossimus sunt provident eve nihil quod quam, ossimus sunt provident eve nihil quod quam",
-    }
-  ]
 
   return (
     <div className={stylesGeneral.container}>
@@ -50,7 +50,10 @@ export default function artigos() {
         {articles.map((article, code) => {
           return (
             <li className={styles.list}>
-              <img src={"/articles/" + article.image} alt="" />
+
+              <div className={styles.image}>
+                <img src={"/articles/" + article.image} alt="" />
+              </div>
               <div className={styles.data}>
                 <p>{article.date}</p>
                 <h2>{article.title}</h2>
