5f70cf237c4a546cd8b8096c49daf8a3b6e05035
João Pedro Alvarenga <joaopedroaats@pm.me>
2020-11-10 13:49:15 -0300

    fix: pwa local error "GenerateSW has been called multiple times"

--- next.config.js
+++ next.config.js
@@ -1,14 +1,20 @@
 const withPWA = require('next-pwa')
 
-module.exports = withPWA({
+const prod = process.env.NODE_ENV === 'production'
+
+const settings = {
   pwa: {
+    disable: prod ? false : true,
     dest: 'public'
   },
-  webpack: function(config) {
+  webpack: (config) => {
     config.module.rules.push({
       test: /\.md$/,
       use: 'raw-loader',
     })
     return config
   },
-})
+}
+
+module.exports = withPWA(settings)
+
